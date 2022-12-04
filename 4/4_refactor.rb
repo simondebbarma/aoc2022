a62c2c8c06770f9baef1451c9ebe9f7ef53529b4
Simon Debbarma <simonpeterdebbarma@outlook.com>
2022-12-04 17:34:49 +0530

    refactor day 4

---
+++ 4/4_refactor.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+range_overlaps = total_overlaps = 0
+
+File.open('a').each do |line|
+  x, y = line.split(',').map { eval _1.sub '-', '..' }
+  range_overlaps += 1 if x.cover?(range2) || y.cover?(range1)
+  total_overlaps += 1 if x.to_a & y.to_a != []
+end
+
+puts range_overlaps
+puts total_overlaps
