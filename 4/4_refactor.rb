d721adac015cc600785d61de1bdad8f8ba02c4d5
Simon Debbarma <simonpeterdebbarma@outlook.com>
2022-12-04 17:41:05 +0530

    RF: remove eval and use Range.new

--- 4/4_refactor.rb
+++ 4/4_refactor.rb
@@ -3,7 +3,7 @@
 range_overlaps = total_overlaps = 0
 
 File.open('a').each do |line|
-  x, y = line.split(',').map { eval _1.sub '-', '..' }
+  x, y = line.split(',').map { |s| Range.new(*s.split('-')) }
   range_overlaps += 1 if x.cover?(range2) || y.cover?(range1)
   total_overlaps += 1 if x.to_a & y.to_a != []
 end
